b66cc3c5bf12ffbf54468489ceb98ea729e143e0
carlosziegler <carlos.ziegler@gmail.com>
2020-01-17 04:49:27 +0100

    ReactJs
    style

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import './Global.css'
+import './App.css'
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      
-    </div>
+      <div id="app">
+        <aside>
+        <strong>Cadastrar</strong>
+        <form action="">
+          <div className="input-block">
+          <label htmlFor="github_username">Usuario do Github</label>
+          <input name="github_username" id="git" required />
+          </div>
+
+          <div className="input-block">
+          <label htmlFor="techs">Technologies</label>
+          <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-block">
+          <label htmlFor="github_username"></label>
+          <input name="github_username" id="git" required />
+          </div>
+
+        </form>
+        </aside>
+        <main>
+
+        </main>
+      </div>
   );
 }
 
